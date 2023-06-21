5016fe56690ddc7d3f7f87afddae8d7149ce0493
Naossk9696 <naooo11496@gmail.com>
2023-06-21 17:22:42 +0900

    Update blink.py

--- blink.py
+++ blink.py
@@ -5,8 +5,7 @@ from flask import Flask, request, redirect, render_template, flash
 from werkzeug.utils import secure_filename
 from tensorflow.keras.models import Sequential,load_model
 from tensorflow.keras.preprocessing import image
-import tensorflow as tf
-from tensorflow import keras
+
 
 classes = ['Jennie', 'Jisoo', 'Lisa', 'Rose']
 image_size = 150
@@ -20,7 +19,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-model =  keras.models.load_model('./model.h5')
+model =  load_model('./model.h5')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -37,6 +36,7 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(UPLOAD_FOLDER, filename))
             filepath = os.path.join(UPLOAD_FOLDER, filename)
+            print(filepath)
 
             img = image.load_img(filepath, target_size=(image_size, image_size))
             img = image.img_to_array(img)
@@ -47,13 +47,14 @@ def upload_file():
 
             pred_answer = 'This member is  ' + classes[predicted] + '.'
 
-            with open(filepath, 'rb') as f:
-                img_base64 = base64.b64encode(f.read())
+            #with open(filepath, 'rb') as f:
+            #    img_base64 = base64.b64encode(f.read())
 
-            return render_template('index.html', answer=pred_answer, img=img_base64)
+            #return render_template('index.html', answer=pred_answer, img=img_base64)
+            return render_template('index.html',answer=pred_answer,imagefile=filepath)
 
-    return render_template('index.html',answer='', img='')
-
+    #return render_template('index.html',answer='', img='')
+    return render_template('index.html',answer='')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
