53473f49b2ad3cb53c4055b6742e832abb471d31
Naossk9696 <naooo11496@gmail.com>
2023-06-21 15:56:25 +0900

    Update blink.py

--- blink.py
+++ blink.py
@@ -18,7 +18,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-model = load_model('BlackPink_faceapp/model.h5')
+model = load_model('./model.h5')
 
 
 @app.route('/', methods=['GET', 'POST'])
